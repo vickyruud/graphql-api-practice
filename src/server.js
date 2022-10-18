d27583de71a3d2d1267f35385f9de6a129f9ffbe
Aprajit Kumar <aprajit.k@gmail.com>
2022-10-18 11:58:52 -0400

    add resolvers for links

--- src/server.js
+++ src/server.js
@@ -5,24 +5,87 @@ const { gql } = require('apollo-server-lambda');
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Mutation {
+    post(url: String!, description: String!): Link!,
+    
+    updateLink( id:String! , url: String!, description: String!): Link!
+  }
+
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+const users = [
+  {
+    id: '1',
+    name: 'Elizabeth Bennet',
+  },
+  {
+    id: '2',
+    name: 'Fitzwilliam Darcy',
+  },
+];
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
   Query: {
-    hello: () => "Hi! Love from @stemmlerjs 🤠."
-  }
-};
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+  
+    let idCount = links.length
 
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      const link = links.find(link => link.id === args.id)
+      if (!link) {
+        throw new Error('Could not find link!!')
+      }
+      link.url = args.url
+      link.description = args.description
+      return link
+    }
+  },
+}
+const corsOptions = {
+    origin: "http://localhost:3000",
+};
+  
 function createLambdaServer () {
   return new ApolloServerLambda({
     typeDefs,
     resolvers,
     introspection: true,
     playground: true,
+    cors: corsOptions
+
   });
 }
+
+
 
 function createLocalServer () {
   return new ApolloServer({
@@ -30,6 +93,7 @@ function createLocalServer () {
     resolvers,
     introspection: true,
     playground: true,
+    cors: corsOptions
   });
 }
 
